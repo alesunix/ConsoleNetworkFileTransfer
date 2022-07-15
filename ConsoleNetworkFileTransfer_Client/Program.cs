af8c60d9b8890c75ac6d37ad69304e9a4995fe4d
Ilyas <alesunix@gmail.com>
2022-07-15 10:27:26 +0600

    Запуск клиента

--- ConsoleNetworkFileTransfer_Client/Program.cs
+++ ConsoleNetworkFileTransfer_Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleNetworkFileTransfer_Client;
 
+Console.WriteLine("Запуск клиента...");
 ClientModel clientModel = new ClientModel();
 clientModel.ConnectToServer(clientModel.serverIP, clientModel.serverPort);
 clientModel.SendFile();
--- ConsoleNetworkFileTransfer_Server/Program.cs
+++ ConsoleNetworkFileTransfer_Server/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleNetworkFileTransfer_Server;
 
-Console.WriteLine("Запуск клиента...");
 ServerModel serverModel = new ServerModel();
 serverModel.StartReceiving();
